826b67efac63a6278328d279303eb9de3e50f1d0
kenkairon <ingeinfo2006@gmail.com>
2022-09-24 20:23:55 -0300

    ejercicio2

---
+++ Documents/actividades/clase_1/main.rb
@@ -0,0 +1,28 @@
+#puts imprime mensaje en pantalla con salto de linea
+puts "hola mundo"
+#print imprime mensaje en pantalla sin salto de linea
+print "hola mundo"
+
+
+
+
+nombre = "Roberto"
+
+puts nombre
+
+#me coloca la primera letra en mayuscula y el resto en minuscula
+puts nombre.capitalize 
+
+#me coloca la cadena con mayuscula
+puts nombre.upcase
+apellido ="carlos"
+puts "hola #{nombre.capitalize} #{apelido.capitalize}."
+
+edad = 40 
+altura = 1.80
+peso = 75
+
+imc = peso / altura**2 
+
+puts "indice de masa corporal#{imc.round()}"
+
